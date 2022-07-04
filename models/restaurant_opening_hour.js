9451ce59b523d8f41258005077edd7d3de1967c7
umairian <itsumairsyed@gmail.com>
2022-07-04 16:06:51 +0500

    restaurant_opening_hours model created

---
+++ models/restaurant_opening_hour.js
@@ -0,0 +1,51 @@
+"use strict";
+const moment = require("moment");
+
+module.exports = (sequelize, DataTypes) => {
+    const RestaurantOpeningHour = sequelize.define("restaurant_opening_hours", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        dish_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DOUBLE,
+            allowNull: false,
+        },
+        fk_restaurant_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    });
+
+    RestaurantOpeningHour.beforeCreate(async (restaurantOpeningHour) => {
+        restaurantOpeningHour.dataValues.createdAt = moment().unix();
+        restaurantOpeningHour.dataValues.updatedAt = moment().unix();
+    });
+    RestaurantOpeningHour.beforeUpdate(async (restaurantOpeningHour) => {
+        restaurantOpeningHour.dataValues.updatedAt = moment().unix();
+    });
+
+    RestaurantOpeningHour.associate = (models) => {
+        RestaurantOpeningHour.belongsTo(models.Restaurants, {
+            as: "restaurant",
+            foreignKey: "fk_restaurant_id",
+            targetKey: "id",
+        });
+    };
+
+    return RestaurantOpeningHour;
+};
