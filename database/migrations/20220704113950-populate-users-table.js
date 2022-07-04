fd81eed28232b54cfb1123e9ab407780294ebfd7
umairian <itsumairsyed@gmail.com>
2022-07-04 17:11:34 +0500

    Create 20220704113950-populate-users-table.js

---
+++ database/migrations/20220704113950-populate-users-table.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const usersData = require("../../data/user");
+const moment = require("moment");
+const table = "users";
+
+module.exports = {
+  up: (queryInterface) => {
+    const usersDataModified = usersData.map(user => ({
+      name: user.name,
+      cash_balance: user.cashBalance,
+      createdAt: moment().unix(),
+      updatedAt: moment().unix()
+    }));
+    return queryInterface.bulkInsert(table, usersDataModified);
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.bulkDelete(table);
+  }
+};
