dc8a454911628733432a31b8a1cbe4bc1f2a1cd6
umairian <itsumairsyed@gmail.com>
2022-07-04 17:11:45 +0500

    Create 20220704111035-create-users-table.js

---
+++ database/migrations/20220704111035-create-users-table.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+
+const table = "users";
+
+module.exports = {
+  up: (queryInterface) => {
+    return queryInterface.createTable(table, {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      cash_balance: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      createdAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable(table);
+  }
+};
