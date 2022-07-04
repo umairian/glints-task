583c0a0b38273c61e8374845ec73fddc04f70046
umairian <itsumairsyed@gmail.com>
2022-07-04 17:11:48 +0500

    Create 20220704111147-create-user-purchase-history-table.js

---
+++ database/migrations/20220704111147-create-user-purchase-history-table.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+
+const table = "user_purchase_history";
+
+module.exports = {
+  up: (queryInterface) => {
+    return queryInterface.createTable(table, {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      fk_restaurant_menu_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      fk_user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      transaction_amount: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      transaction_date: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable(table);
+  }
+};
