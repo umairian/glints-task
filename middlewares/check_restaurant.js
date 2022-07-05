042da1ce267d4d18aaa1e45f5cf97c0d7305d32a
umairian <itsumairsyed@gmail.com>
2022-07-05 20:07:20 +0500

    middlewares created

---
+++ middlewares/check_restaurant.js
@@ -0,0 +1,17 @@
+const { isNull } = require("lodash");
+const { Restaurants } = require("../models");
+
+module.exports = async (req, res, next) => {
+    try {
+        const { restaurantId } = req.params;
+        const restaurant = await Restaurants.findByPk(restaurantId);
+        if(isNull(restaurant)) {
+            throw { status: 404, message: "Restaurant not found" };
+        }
+        req.restaurant = restaurant;
+        next();
+    } catch (err) {
+        console.log(err);
+        return res.status(err.status || 500).send(err.message || "Something went wrong!");
+    }
+}
